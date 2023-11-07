22815ce120594ef0fe227a6a4ef6eec83f041bff
Renansilsan <141248258+Renansilsan@users.noreply.github.com>
2023-11-07 14:14:18 -0300

    Upload

---
+++ script/script.js
@@ -0,0 +1,19 @@
+function carregar() {
+    var msg = window.document.getElementById(`msg`)
+    var img = window.document.getElementById(`imagem`)
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 0 && hora < 12) {
+        img.src = 'imagens/foto-manha.jpg'
+        document.body.style.background =  '#97bff3'
+    }
+    else if (hora >= 12 && hora < 18) {
+        img.src = 'imagens/foto-tarde.jpg'
+        document.body.style.background = '#d49216'
+    }
+    else {
+        img.src = 'imagens/foto-noite.jpg'
+        document.body.style.background = '#020220'
+    }
+}
